4fa788304c5128b2f33f32d570ead4ebfea41cf0
eulerBM <eullerborgesdamotta155@gmail.com>
2022-04-16 20:08:30 -0300

    first commit

---
+++ core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from core.models import Evento
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('titulo', 'data_evento',)
+
+admin.site.register(Evento, EventoAdmin)
+
+# Register your models here.
